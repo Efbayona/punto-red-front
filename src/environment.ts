bb009382219085ac1d1abfbf61985fac95a4ce12
Edwin <efbayonaa@ufpso.edu.co>
2025-08-14 21:10:00 -0500

    Adjust deploy.

--- src/environment.ts
+++ src/environment.ts
@@ -2,5 +2,6 @@
 export const environment = {
 
     /* --- Url backend ---- */
-    api: 'http://localhost:8080/app',
+    // api: 'http://localhost:8080/app',
+     api: 'https://punto-red-btbxf9dheqcbb5hb.eastus2-01.azurewebsites.net/app'
 }
